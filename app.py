fe73df926ff408cf12b975629bedba33aef12ffe
Vishal Nagar <150803470+Vishalnagar410@users.noreply.github.com>
2025-01-04 18:02:17 +0000

    html file added and rendered with flask

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from re import DEBUG
 from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    #return render_template('index.html')
-    return "hello world"
+    return render_template('index.html')
 
-if __name__=='__main__':
-  app.run(host='0.0.0.0',port=8080, debug=True)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
