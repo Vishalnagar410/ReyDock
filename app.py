ab23750c16f9284f557aec45200ad85ba8bae264
Vishal Nagar <150803470+Vishalnagar410@users.noreply.github.com>
2025-01-06 10:39:52 +0000

    Added HTML and CSS for webpage

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 
+# ... rest of your code
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+#@app.route('/api/protien/')
+#def protien():
+#return render_template('protien.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
